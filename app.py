378ec3a25f8e0ab2f63427e08d08eabc24bd1991
nattesharan <nattesharan@gmail.com>
2017-04-29 18:17:38 +0530

    added flask for messaging

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for,request,render_template,make_response,abort
+from flask import Flask,redirect,url_for,request,render_template,make_response,abort,flash
 import random
 app = Flask(__name__)
 secret = ''
@@ -98,5 +98,18 @@ def signin():
 def logout():
     session.pop('username', None)
     return redirect(url_for('initial'))
+@app.route('/flash')
+def flash():
+    return render_template('index.html')
+@app.route('/signinn', methods = ['GET', 'POST'])
+def signinn():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid username or password. Please try again!'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('flash'))
+    return render_template('login.html', error = error)
 if __name__ == '__main__':
    app.run(port=int("3000"),debug = True)
